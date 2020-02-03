5e696b3998b0eacfb3a9766dea61afaa9a24a2ce
bingsenh <bingsenh@163.com>
2020-02-03 20:03:41 +0800

    update 实现页面缓存和对象缓存

--- src/main/java/com/bingsenh/seckill/controller/GoodsController.java
+++ src/main/java/com/bingsenh/seckill/controller/GoodsController.java
@@ -1,17 +1,25 @@
 package com.bingsenh.seckill.controller;
 
+import com.alibaba.druid.util.StringUtils;
 import com.bingsenh.seckill.domain.Goods;
 import com.bingsenh.seckill.domain.MiaoshaUser;
+import com.bingsenh.seckill.redis.GoodsKey;
 import com.bingsenh.seckill.redis.RedisService;
 import com.bingsenh.seckill.service.GoodsService;
 import com.bingsenh.seckill.service.MiaoshaUserService;
 import com.bingsenh.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.spring4.context.SpringWebContext;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -27,19 +35,49 @@ public class GoodsController {
     @Autowired
     GoodsService goodsService;
 
-    @RequestMapping("/to_list")
-    public String list(Model model, MiaoshaUser user){
+    @Autowired
+    RedisService redisService;
+
+    @Autowired
+    ThymeleafViewResolver thymeleafViewResolver;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+
+    @RequestMapping(value = "/to_list",produces = "text/html")
+    @ResponseBody
+    public String list(HttpServletRequest request, HttpServletResponse response,Model model, MiaoshaUser user){
         model.addAttribute("user",user);
-        //查询商品列表
+        // 查询商品列表
+        // 取缓存
+        String html = redisService.get(GoodsKey.getGoodsList,"",String.class);
+        if(!StringUtils.isEmpty(html)){
+            return html;
+        }
         List<GoodsVo> goodsList = goodsService.listGoodsVo();
         model.addAttribute("goodsList",goodsList);
-        return "goods_list";
+        // 手动渲染
+        SpringWebContext ctx = new SpringWebContext(request,response,request.getServletContext(),request.getLocale(),model.asMap(),applicationContext);
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list",ctx);
+        if(!StringUtils.isEmpty(html)){
+            redisService.set(GoodsKey.getGoodsList,"",html);
+        }
+        return html;
+        //return "goods_list";
     }
 
-    @RequestMapping("/to_detail/{goodsId}")
-    public String detail(Model model, MiaoshaUser user, @PathVariable("goodsId") long goodsId){
-
+    @RequestMapping(value = "/to_detail/{goodsId}",produces = "text/html")
+    @ResponseBody
+    public String detail(HttpServletRequest request,HttpServletResponse response,Model model, MiaoshaUser user, @PathVariable("goodsId") long goodsId){
         model.addAttribute("user",user);
+        // 取缓存
+        String html = redisService.get(GoodsKey.getGoodsDetail,""+goodsId,String.class);
+        if(!StringUtils.isEmpty(html)){
+            return html;
+        }
+
+        // 手动渲染
         GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
         model.addAttribute("goods",goods);
 
@@ -62,7 +100,14 @@ public class GoodsController {
         model.addAttribute("miaoshaStatus",miaoshaStatus);
         model.addAttribute("remainSeconds",remainSeconds);
 
-        return "goods_detail";
+        SpringWebContext ctx = new SpringWebContext(request,response,request.getServletContext(),request.getLocale(),model.asMap(),applicationContext);
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_detail",ctx);
+        if(!StringUtils.isEmpty(html)){
+            redisService.set(GoodsKey.getGoodsDetail,""+goodsId,html);
+        }
+        return html;
+
+        //return "goods_detail";
 
     }
 
--- src/main/java/com/bingsenh/seckill/service/MiaoshaUserService.java
+++ src/main/java/com/bingsenh/seckill/service/MiaoshaUserService.java
@@ -31,7 +31,43 @@ public class MiaoshaUserService {
     @Autowired
     RedisService redisService;
 
-    public boolean login(HttpServletResponse response,LoginVo loginVo){
+    public MiaoshaUser getById(long id){
+        // 取缓存
+        MiaoshaUser user = redisService.get(MiaoshaUserKey.getById,""+id,MiaoshaUser.class);
+        if(user!=null){
+            return user;
+        }
+
+        // 取数据库
+        user = miaoshaUserDao.getById(id);
+        if(user!=null){
+            redisService.set(MiaoshaUserKey.getById,""+id,user);
+        }
+        return  user;
+
+    }
+
+    public boolean updatePassword(String token,long id,String fromPass){
+        // 取user
+        MiaoshaUser user = getById(id);
+        if(user == null){
+            throw new GlobalException(CodeMsg.MOBILE_NOT_EXIST);
+        }
+
+        // 更新数据库
+        MiaoshaUser toBeUpdate = new MiaoshaUser();
+        toBeUpdate.setId(id);
+        toBeUpdate.setPassword(MD5Util.fromPassToDbPass(fromPass,user.getSalt()));
+        miaoshaUserDao.update(toBeUpdate);
+
+        // 处理缓存
+        redisService.delete(MiaoshaUserKey.getById,""+id);
+        user.setPassword(toBeUpdate.getPassword());
+        redisService.set(MiaoshaUserKey.token,token,user);
+        return true;
+    }
+
+    public String login(HttpServletResponse response,LoginVo loginVo){
         if(loginVo == null){
             throw new GlobalException(CodeMsg.SERVER_ERROR);
         }
@@ -56,7 +92,7 @@ public class MiaoshaUserService {
         String token = UUIDUtil.uuid();
         addCookie(response,token,user);
 
-        return true;
+        return token;
     }
 
 
